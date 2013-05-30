9b9f6057f745bad45d89c6eb83dd50b6980221fe
intekhab <mohammed_intekhab@hotmail.com>
2013-05-30 18:03:58 +0530

    another modification

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ function p($var) {
 	echo '</pre>';
 
 }
+
+
 /*
  * CURRENT : Current Working Directory
  * SEP : Directory Seprator
@@ -15,17 +17,36 @@ if (!defined('CURRENT')) {
 	define('CURRENT', getcwd());
 }
 if (!defined('SEP')) {
-	define('SEP', '/');
+	define('SEP', '\\');
 }
 if (!defined('BASEFOLDER')) {
-	 $url=explode('\\', CURRENT);
-	define('BASEFOLDER', $url[count($url)-1]);
+	$url = explode('\\', CURRENT);
+	define('BASEFOLDER', $url[count($url) - 1]);
 }
-p(MapUrl());
-function MapUrl(){
-$keywords = preg_split('/'.BASEFOLDER.'\//',$_SERVER['REQUEST_URI']);
-p($keywords);
-return explode('/',$keywords[1]);
+if (!defined('APPFOLDER')) {
+	
+	define('APPFOLDER','web');
 }
+if (!defined('EXTENSION')) {
+	
+	define('EXTENSION','.php');
+}
+UrlDeep();
+function UrlDeep(){
+	$exploded_url=MapUrl();
+	p($exploded_url);
+	
+}
+/*
+ * Function MapUrl
+ * This Function is used to get the URL Patterns easiliy
+ * Created date 30/5/2013
+ */
+function MapUrl() {
+	$keywords = preg_split('/' . BASEFOLDER . '\//', $_SERVER['REQUEST_URI']);
+	//p($keywords);
+	return explode('/', $keywords[1]);
+}
+require_once(CURRENT.SEP.APPFOLDER.SEP.'base'.EXTENSION);
 
 ?>
---
+++ web/base.php
@@ -0,0 +1,14 @@
+<?php
+class base{
+	var $page;
+	var $controller;
+	var $action;
+	var $parameteres;
+	var $key;
+	/*
+	 * Function for dispatching
+	 * created 30-5-2013
+	 */
+
+}
+ ?>
