cce244d6a9d56ccf03758503be47de9ef5664f49
intekhab <mohammed_intekhab@hotmail.com>
2013-06-01 18:31:38 +0530

    Done today

--- web/controller/users.php
+++ web/controller/users.php
@@ -6,7 +6,7 @@ class Users extends Controller {
 	function includer($parameters,$action,$local_controller,$link_function=null) {
 			try{
 				require_once (CURRENT . SEP . APPFOLDER . SEP . 'functions' . SEP .$local_controller.SEP.$action. EXTENSION);
-				
+			
 				call_user_func_array($action,$parameters);
 			}catch(Exception $e){
 				echo 'Caught exception: ', $e -> getMessage(), "\n";
